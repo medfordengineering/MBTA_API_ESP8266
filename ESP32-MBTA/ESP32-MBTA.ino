f1f90c5e3cf2594250593db7d02556811d436066
medfordengineering <schristy@medford.k12.ma.us>
2024-03-03 21:02:43 -0500

    condensed working version

--- ESP32-MBTA/ESP32-MBTA.ino
+++ ESP32-MBTA/ESP32-MBTA.ino
@@ -59,44 +59,31 @@ void setup() {
 
 void loop() {
 
-  // WiFiClientSecure* client = new WiFiClientSecure;
   WiFiClientSecure client;
-  if (true || client) {
-    // set secure client with certificate
-    // client->setCACert(rootCACertificate);
-    client.setCACert(rootCACertificate);
-    // client.setTru
-    //create an HTTPClient instance
-    HTTPClient https;
-    Serial.println(api_url + "&api_key=" + api_key);
+  client.setCACert(rootCACertificate);
+  HTTPClient https;
 
-    //Initializing an HTTPS communication using the secure client
-    Serial.print("[HTTPS] begin...\n");
-    if (https.begin(client,  api_url+"&api_key="+api_key)) {  // HTTPS
-    // if (https.begin(*client, api_url)) {  // HTTPS
-      Serial.print("[HTTPS] GET...\n");
-      // start connection and send HTTP header
-      int httpCode = https.GET();
-      // httpCode will be negative on error
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
-        // file found at server
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          // print server response payload
-          String payload = https.getString();
-          Serial.println(payload);
-        }
-      } else {
-        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+  if (https.begin(client, api_url + "&api_key=" + api_key)) { 
+   
+    int httpCode = https.GET();
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        // print server response payload
+        String payload = https.getString();
+        Serial.println(payload);
       }
-      https.end();
+    } else {
+      Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
     }
-  } else {
-    Serial.printf("[HTTPS] Unable to connect\n");
+    https.end();
   }
+
   Serial.println();
-  Serial.println("Waiting 2min before the next round...");
+  // Serial.println("Waiting 2min before the next round...");
   // delete client;
-  delay(250);
+  delay(3000);
 }
